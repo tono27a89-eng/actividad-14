7f3b45e9914eb33d78aa7cd2fb3e759e122d2c97
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-18 15:16:34 -0600

    se me volvio a olvidar

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,7 +1,29 @@
 ﻿bool validar = false;
+List<Empleado> empleados = new List<Empleado>();
 while (validar != true)
 {
     Console.WriteLine("escriba la cantidad de empleados a Registrar:");
+    int.TryParse(Console.ReadLine(), out int lim);
+    if (lim > 0)
+    {
+        validar = true;
+        for (int i = 0; i < lim; i++)
+        {
+            Empleado e=new Empleado();
+            Console.WriteLine("ingrese nombre: ");e.nombre = Console.ReadLine();
+            Console.WriteLine("ingrese puesto: ");e.puesto = Console.ReadLine();
+            Console.WriteLine("ingrese salario: ");e.salario= double.Parse(Console.ReadLine());
+            empleados.Add(e);            
+        }
+        foreach (Empleado e in empleados) 
+        {
+            Console.WriteLine($"empleado: {e.nombre} | puesto: {e.puesto} | salario anual: {e.SalarioAnual()+e.Bono()}"); 
+        }
+    }
+    else
+    {
+        Console.WriteLine("ingrese valor valido");
+    }
 }
 class Empleado()
 {
---
+++ 04/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Security.Cryptography;
+
+List<Libro> libros = new List<Libro>();
+bool validar=false;
+while (validar != true)
+{
+    Console.WriteLine("ingrese la cantidad de libros a Registrar: ");
+    int.TryParse(Console.ReadLine(), out int lim);
+    if (lim>0)
+    {
+        validar = true;
+        for (int i = 0; i < lim; i++)
+        {
+            Libro l = new Libro();
+            Console.WriteLine("ingrese titulo: ");l.titulo = Console.ReadLine();
+            Console.WriteLine("ingrese autor: ");l.autor = Console.ReadLine();
+            Console.WriteLine("ingrese categoria: ");l.categoria = Console.ReadLine();
+            Console.WriteLine("ingrese numero de paginas: ");l.paginas=int.Parse(Console.ReadLine());
+            libros.Add(l);
+        }
+        Libro mayor = libros[0];
+        foreach (Libro l in libros)
+        {
+            l.Mostrar();
+            if (l.paginas> mayor.paginas) {mayor=l; }
+        }
+        Console.WriteLine("el libro con mayor paginas es: "+mayor.titulo);
+        mayor.Mostrar();
+    }
+    else
+    {
+        Console.WriteLine("ingrese valor valido");
+    }
+}
+
+
+class Libro()
+{
+    public string titulo;
+    public string autor;
+    public string categoria;
+    public int paginas;
+    public string Clasificar()
+    {
+        if (paginas < 300) { return "novela ligera"; }
+        else if (paginas < 450) { return "novela"; }
+        else { return "libro o novela larga"; }
+    }
+    public void Mostrar()
+    {
+        Console.WriteLine($"titulo: {titulo}| autor: {autor}| categoria: {categoria} | paginas: {paginas}| tipo:{Clasificar()}");
+    }
+}
