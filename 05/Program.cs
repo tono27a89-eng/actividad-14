fe5baa28dfd98e1d3ff00a507af01b2a3a281983
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-18 16:20:55 -0600

    prueba

--- 05/Program.cs
+++ 05/Program.cs
@@ -1,8 +1,35 @@
-﻿
+﻿using System.Security.Cryptography;
+
+
 List<Ventas> venta = new List<Ventas>();
 bool validar=false;
 while (validar!= true)
 {
+    Console.WriteLine("ingrese cantidad de Ventas: ");
+    int.TryParse(Console.ReadLine(), out int lim);
+    if(lim > 0)
+    {
+        validar = true;
+        for (int i = 0; i < lim; i++)
+        {
+            Ventas v = new Ventas();
+            Console.WriteLine($"producto No.{i+1}: ");v.producto = Console.ReadLine();
+            Console.WriteLine("precio: ");v.precio= double.Parse(Console.ReadLine());
+            Console.WriteLine("cantidad: ");v.cantidad= double.Parse(Console.ReadLine());
+            venta.Add(v);
+        }
+        double totalventas = 0;
+        foreach (Ventas v in venta)
+        {
+            v.Mostrar();
+            totalventas += v.Total();
+        }
+        Console.WriteLine("total de ventas es: "+totalventas);
+    }
+    else
+    {
+        Console.WriteLine("ingresar valor valido");
+    }
     
 }
 class Ventas ()
@@ -11,4 +38,13 @@ class Ventas ()
     public double precio;
     public double cantidad;
     public double Subtotal() { return cantidad * precio; }
+    public double Descuento() { if (Subtotal() > 500) { return Subtotal() * 0.05; }
+        else if (Subtotal() > 1000) { return Subtotal() * 0.10; }
+        else { return 0; }
+    }
+    public double Total() { return Subtotal() - Descuento(); }
+    public void Mostrar()
+    {
+        Console.WriteLine($"Venta de: {producto} | subtotal: {Subtotal()} | Descuento: {Descuento()} | total: {Total()}");
+    }
 }
