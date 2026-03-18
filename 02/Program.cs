acf74659e5a80cc6911920a34cfd528cd488ef2d
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-18 09:55:06 -0600

    comienzo

---
+++ 02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+Console.WriteLine("ingrese la cantidad de productos a Registrar: ");
+int.TryParse(Console.ReadLine(), out int lim);
+class Producto
+{
+    public string nombre;
+    public double precio;
+    public double cantidad;
+    public double Valortotal() { return precio * cantidad; }
+    public string Estado()
+    {
+        if (cantidad <= 10) { return "sin existencia"; }
+        else if (cantidad > 10 && cantidad < 100) { return "stock bajo"; }
+        else { return "stock suficiente"; }
+    }
+}
