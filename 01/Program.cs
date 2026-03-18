25a3b1ac5ca86ba21bb7a9d5454b1d1bdcbc819a
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-18 09:39:40 -0600

    pruba

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,22 +1,38 @@
 ﻿Console.WriteLine("ingrese la cantidad de estudiantes a registrar: ");
 int.TryParse(Console.ReadLine(), out int lim);
+Console.WriteLine(lim);
 List<Estudiante>estudiantes = new List<Estudiante>();
-for (int i = 0; i < lim; i++)
+if (lim > 0)
 {
-  Estudiante e = new Estudiante();
-    Console.WriteLine("ingrese nombre: ");e.nombre = Console.ReadLine();
-    Console.WriteLine("ingrese nota1: ");e.nota1= int.Parse(Console.ReadLine());
-    Console.WriteLine("ingrese nota2: ");e.nota2 = int.Parse(Console.ReadLine());
-    Console.WriteLine("ingrese nota3: ");e.nota3 = int.Parse(Console.ReadLine());
-    estudiantes.Add(e);
+    for (int i = 0; i < lim; i++)
+    {
+        Estudiante e = new Estudiante();
+        Console.WriteLine("ingrese nombre: "); e.nombre = Console.ReadLine();
+        Console.WriteLine("ingrese nota1: "); e.nota1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("ingrese nota2: "); e.nota2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("ingrese nota3: "); e.nota3 = int.Parse(Console.ReadLine());
+        estudiantes.Add(e);
+    }
+    double sumapromedio = 0;
+    Estudiante mejor = estudiantes[0];
+    foreach (Estudiante e in estudiantes)
+    {
+        e.Mostrar();
+        sumapromedio += e.CalcularPromedio();
+        if (mejor.CalcularPromedio()<e.CalcularPromedio())
+        {
+            mejor = e;
+        }
+    }
+    Console.WriteLine($"el promedio total es: {sumapromedio/estudiantes.Count}");
+    Console.WriteLine("mejor estudiante es: ");
+    mejor.Mostrar();
 }
-double sumapromedio = 0;
-Estudiante mejor = estudiantes[0];
-foreach (Estudiante e in estudiantes)
+else
 {
-    e.Mostrar();
-    sumapromedio += e.CalcularPromedio();
+    Console.WriteLine("ingrese valor valido");
 }
+
 class Estudiante
 {
     public string nombre;
