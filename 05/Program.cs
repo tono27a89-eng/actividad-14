78e0d1316d7fc710aee4b829ea2939996ab247da
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-18 15:39:23 -0600

    comenzando

---
+++ 05/Program.cs
@@ -0,0 +1,14 @@
+﻿
+List<Ventas> venta = new List<Ventas>();
+bool validar=false;
+while (validar!= true)
+{
+    
+}
+class Ventas ()
+{
+    public string producto;
+    public double precio;
+    public double cantidad;
+    public double Subtotal() { return cantidad * precio; }
+}
