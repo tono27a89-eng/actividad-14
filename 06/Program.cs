55441ea1010d4a5862a35c3f53e6268556976a69
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-19 09:27:36 -0600

    06

---
+++ 06/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Dictionary<int, Estudiante> estudiantes = new Dictionary<int, Estudiante>();
+bool validar
+class Estudiante
+{
+    public string nombre;
+    public string carrera;
+    public double nota;
+    public string Estado() { return nota >= 61 ? "aprobado" : "reprobado"; }
+    public void Mostrar()
+    {
+        Console.WriteLine($"Nombre: {nombre} | Carrera: {carrera}| nota: {nota}"); 
+    }
+}
